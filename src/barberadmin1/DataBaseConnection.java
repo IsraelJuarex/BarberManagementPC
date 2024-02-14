8941726d52b938314497d5ee8468964d28a5ee85
IsraelJuarex <israeljuarex@gmail.com>
2024-02-14 13:45:07 -0600

    Update DataBaseConnection.java

--- src/barberadmin1/DataBaseConnection.java
+++ src/barberadmin1/DataBaseConnection.java
@@ -16,7 +16,6 @@ class DataBaseConnection {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-         // return DriverManager.getConnection("jdbc:mysql://aws.connect.psdb.cloud/testonlinebd?sslMode=VERIFY_IDENTITY", "qthu0979b5ni6h63ytd3", "pscale_pw_n8Ngp6giqG4oEdJkMYeZSUeswc2yNtHPDDCYqes3gAm");
             return DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_BarberManagement1","freedb_Israel1234","!Tuz??V@C2qq&gv");  
         } catch (Exception var1) {  
            // System.out.println("Error connecting to database: " + var1.getMessage());
